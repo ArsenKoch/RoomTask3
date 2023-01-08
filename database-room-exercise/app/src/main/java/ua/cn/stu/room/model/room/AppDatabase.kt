e58a8d367ff4d2f72df163f1cbe6691a9f894e55
ArsenKoch <arsenkocaran@icloud.com>
2023-01-08 21:17:59 +0300

    Composite primary key; foreign keys

--- database-room-exercise/app/src/main/java/ua/cn/stu/room/model/room/AppDatabase.kt
+++ database-room-exercise/app/src/main/java/ua/cn/stu/room/model/room/AppDatabase.kt
@@ -4,11 +4,15 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import ua.cn.stu.room.model.accounts.room.AccountsDao
 import ua.cn.stu.room.model.accounts.room.entities.AccountDbEntity
+import ua.cn.stu.room.model.boxes.room.entities.AccountBoxSettingDbEntity
+import ua.cn.stu.room.model.boxes.room.entities.BoxDbEntity
 
 @Database(
     version = 1,
     entities = [
-        AccountDbEntity::class
+        AccountDbEntity::class,
+        BoxDbEntity::class,
+        AccountBoxSettingDbEntity::class
     ]
 )
 abstract class AppDatabase : RoomDatabase() {
