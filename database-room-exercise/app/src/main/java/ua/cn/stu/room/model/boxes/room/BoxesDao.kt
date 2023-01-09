74f7cfdd6d96a59e74dd004806df61a9488fc73d
ArsenKoch <arsenkocaran@icloud.com>
2023-01-09 15:55:58 +0300

    Implementation of BoxesDao, fetch data from two tables (LEFT JOIN)

--- database-room-exercise/app/src/main/java/ua/cn/stu/room/model/boxes/room/BoxesDao.kt
+++ database-room-exercise/app/src/main/java/ua/cn/stu/room/model/boxes/room/BoxesDao.kt
@@ -1,11 +1,18 @@
 package ua.cn.stu.room.model.boxes.room
 
-// todo #17: Create a DAO interface for working boxes & settings.
-//           - annotate this interface with @Dao
-//           - add method for activating/deactivating box for the specified account;
-//             hint: method should have 1 argument of AccountBoxSettingDbEntity type and should
-//                   be annotated with @Insert(onConflict=OnConflictStrategy.REPLACE);
-//           - add method for fetching boxes and their settings by account ID;
-//             hint: there are at least 4 options, the easiest one is to
-//                   use Map<BoxDbEntity, AccountBoxSettingDbEntity> as a return type
-interface BoxesDao
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import ua.cn.stu.room.model.boxes.room.entities.AccountBoxSettingDbEntity
+import ua.cn.stu.room.model.boxes.room.entities.BoxDbEntity
+
+@Dao
+interface BoxesDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun setActiveFlagForBox(accountBoxSettingDbEntity: AccountBoxSettingDbEntity)
+
+    @Query("SELECT * FROM boxes LEFT JOIN account_box_settings ON boxes.id = account_box_settings.account_id = :accountId")
+    fun getBoxesAndSettings(accountId: Long): kotlinx.coroutines.flow.Flow<Map<BoxDbEntity, AccountBoxSettingDbEntity?>>
+}
