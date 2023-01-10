4a5cb4f94f0a2fe3c5bae1f33452a592e85e3f24
ArsenKoch <arsenkocaran@icloud.com>
2023-01-10 23:50:00 +0300

    Create an AppDatabase instance by using Room.databaseBuilder()

--- database-room-exercise/app/src/main/java/ua/cn/stu/room/Repositories.kt
+++ database-room-exercise/app/src/main/java/ua/cn/stu/room/Repositories.kt
@@ -16,11 +16,9 @@ object Repositories {
 
     private lateinit var applicationContext: Context
 
-    // -- stuffs
-
     private val database: AppDatabase by lazy<AppDatabase> {
-        TODO("#21: Create an AppDatabase instance by using Room.databaseBuilder static method. " +
-                "Use createFromAssets method to initialize a new database from the pre-packaged SQLite file from assets")
+        Room.databaseBuilder(applicationContext, AppDatabase::class.java, "database.db")
+            .build()
     }
 
     private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
@@ -32,11 +30,19 @@ object Repositories {
     // --- repositories
 
     val accountsRepository: AccountsRepository by lazy {
-        RoomAccountsRepository(TODO("#22: Use AccountsDao here from AppDatabase"), appSettings, ioDispatcher)
+        RoomAccountsRepository(
+            TODO("#22: Use AccountsDao here from AppDatabase"),
+            appSettings,
+            ioDispatcher
+        )
     }
 
     val boxesRepository: BoxesRepository by lazy {
-        RoomBoxesRepository(accountsRepository, TODO("#23: Use BoxesDao here from AppDatabase"), ioDispatcher)
+        RoomBoxesRepository(
+            accountsRepository,
+            TODO("#23: Use BoxesDao here from AppDatabase"),
+            ioDispatcher
+        )
     }
 
     /**
