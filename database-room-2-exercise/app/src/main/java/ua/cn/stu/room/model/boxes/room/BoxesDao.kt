dccf1b564480b93e91f4f2f181018271d4c82160
ArsenKoch <arsenkocaran@icloud.com>
2023-01-12 23:51:48 +0300

    Rewrite getBoxesAndSettings method again: use database view instead of complex SQL-query

--- database-room-2-exercise/app/src/main/java/ua/cn/stu/room/model/boxes/room/BoxesDao.kt
+++ database-room-2-exercise/app/src/main/java/ua/cn/stu/room/model/boxes/room/BoxesDao.kt
@@ -6,21 +6,13 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 import ua.cn.stu.room.model.boxes.room.entities.AccountBoxSettingDbEntity
-import ua.cn.stu.room.model.boxes.room.entities.BoxAndSettingsTuple
-import ua.cn.stu.room.model.boxes.room.entities.BoxDbEntity
+import ua.cn.stu.room.model.boxes.room.views.SettingsDbView
 
 @Dao
 interface BoxesDao {
 
-    @Query(
-        "SELECT * " +
-                "FROM boxes " +
-                "LEFT JOIN accounts_boxes_settings " +
-                "  ON boxes.id = accounts_boxes_settings.box_id AND accounts_boxes_settings.account_id = :accountId"
-    )
-    fun getBoxesAndSettings(accountId: Long): Flow<List<BoxAndSettingsTuple>>
-
-    // todo #11: Rewrite getBoxesAndSettings method again: use database view instead of complex SQL-query.
+    @Query("SELECT * FROM settings_view WHERE account_id = :accountId")
+    fun getBoxesAndSettings(accountId: Long): Flow<List<SettingsDbView>>
 
     // todo #15: Rewrite getBoxesAndSettings method again: Use SettingWithEntitiesTuple which
     //           represents joined data from the database view, 'accounts' and 'boxes' tables
